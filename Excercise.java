1f661ba1df5d5eaf5a50f47aeead8a806a145854
kumamoai <kumamoai0820@gmail.com>
2021-08-18 20:59:00 +0900

    10進数⇒2進数　変換

--- Excercise.java
+++ Excercise.java
@@ -5,32 +5,25 @@ public class Excercise
     public static void main( String[] args ) throws IOException
     {
         BufferedReader br = new BufferedReader( new InputStreamReader( System.in ) );
-        //int l = 1; //loop
-        //int max =0;
-        //int min =0;
-        //int a =1; //０が入力されたかの判定用
-        //int one =0; //一つ目の数
-        //int two =1; //二つ目の数
-        int[] v =new int[10];
+ 
+        //入力された数値を 16 桁の２進数で表示するプログラム
 
-        //int sum =0;
-        //double Count =0;
-        int sum =0;
-        int sum2 =0;
-        //int b =0;
-        //int f =0;
 
-        for( int i =0  ; i < 10 ; i++ ){
-            System.out.print((i+1)+"番目の入力は？＞");
-            v[i] = Integer.parseInt( br.readLine());  
-            sum += v[i];
-            if(sum>=100||i==9){
-                for(i =0; i<10;i++){
-                    sum2 += v[i];
-                }
-            }
+        System.out.println("10進数をどうぞ");        
+        int value = Integer.parseInt( br.readLine() );
+        int binary[] = new int[16];
+
+        // 配列 binaryに0か1を代入する
+        for( int i = 0 ; i < 16 ; i++ ){
+            binary[i]=value%2;
+            value /= 2;
         }
-        System.out.println(sum2);
+        
+        System.out.println("2進数にするよ");
+        for( int i = 15 ; i >= 0 ; i-- ){
+            System.out.print( binary[i] );
+        }
+        
 
         
     }
