43a73e024215f2ddf9fc8496a83285c67e19590b
kumamoai <kumamoai0820@gmail.com>
2021-08-19 20:33:14 +0900

    九九表から計算結果出力

--- Excercise.java
+++ Excercise.java
@@ -4,9 +4,16 @@ public class Excercise
 {
     public static void main( String[] args ) throws IOException
     {
-        //BufferedReader br = new BufferedReader( new InputStreamReader( System.in ) );
+        BufferedReader br = new BufferedReader( new InputStreamReader( System.in ) );
  
         //  九九表プログラム
+        int a=0;
+        int b=0;
+
+        System.out.print("入力１＞");
+        a = Integer.parseInt( br.readLine() );
+        System.out.print("入力２＞");
+        b = Integer.parseInt( br.readLine() );
 
         int kuku[][] = new int[9][9];
         // 配列 kuku に値を代入する
@@ -14,12 +21,11 @@ public class Excercise
             for( int j = 0 ; j < 9 ; j++ ){
                 kuku[i][j]=(i+1)*(j+1);
 
-                System.out.printf("%2d", kuku[i][j]);
-                System.out.print(" ");
-        
+                //System.out.printf("%2d", kuku[i][j]);
+                //System.out.print(" ");        
             }
-            System.out.println();
-
+            //System.out.println();
         }
+        System.out.println(kuku[a-1][b-1]);
     }
 }   
