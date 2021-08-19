f1036e2a0e3507c527ec116eddd9b54187ec3774
kumamoai <kumamoai0820@gmail.com>
2021-08-19 20:17:16 +0900

    九九表　表示　（二次元配列）

--- Excercise.java
+++ Excercise.java
@@ -4,27 +4,22 @@ public class Excercise
 {
     public static void main( String[] args ) throws IOException
     {
-        BufferedReader br = new BufferedReader( new InputStreamReader( System.in ) );
+        //BufferedReader br = new BufferedReader( new InputStreamReader( System.in ) );
  
-        //入力された数値を 16 桁の２進数で表示するプログラム
+        //  九九表プログラム
 
+        int kuku[][] = new int[9][9];
+        // 配列 kuku に値を代入する
+        for( int i = 0 ; i < 9 ; i++ ){
+            for( int j = 0 ; j < 9 ; j++ ){
+                kuku[i][j]=(i+1)*(j+1);
 
-        System.out.println("10進数をどうぞ");        
-        int value = Integer.parseInt( br.readLine() );
-        int binary[] = new int[16];
+                System.out.printf("%2d", kuku[i][j]);
+                System.out.print(" ");
+        
+            }
+            System.out.println();
 
-        // 配列 binaryに0か1を代入する
-        for( int i = 0 ; i < 16 ; i++ ){
-            binary[i]=value%2;
-            value /= 2;
         }
-        
-        System.out.println("2進数にするよ");
-        for( int i = 15 ; i >= 0 ; i-- ){
-            System.out.print( binary[i] );
-        }
-        
-
-        
     }
 }   
