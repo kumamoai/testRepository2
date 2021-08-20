18ceb2f26041c13a2d8860cca3036fa11059517d
kumamoai <kumamoai0820@gmail.com>
2021-08-21 07:49:45 +0900

    3つの数でもできるように

--- Excercise.java
+++ Excercise.java
@@ -3,20 +3,25 @@ import java.io.*;
 
 public class Excercise
 {
-    
+    //3つ以上の数でもできるように
+
     public static void main( String[] args )throws IOException{
-        //System.out.println( "3の2乗=" + pow2( 3 ) );
-        //System.out.println("平均は"+ ave(3,15));
-        System.out.println("大きいほうは"+big(23,32));
+
+        BufferedReader br = new BufferedReader(
+                            new InputStreamReader( System.in ) );
+        int x =Integer.parseInt(br.readLine());
+        int y =Integer.parseInt(br.readLine());
+        int z =Integer.parseInt(br.readLine());
+        System.out.println("一番大きいのは"+big(big(x,y),z));
 
     }
    
-    static int big(int x , int y)
+    static int big(int n , int m)
     {
-        if(x>y){
-            return x;
+        if(n>m){
+            return n;
         }else{
-            return y;
+            return m;
         }        
     }
 }
