c331becab3fbe274a75717e637aa201491287af5
kumamoai <kumamoai0820@gmail.com>
2021-08-24 23:39:33 +0900

    九九出力メソッド

--- Excercise.java
+++ Excercise.java
@@ -3,35 +3,30 @@ import java.util.Scanner;
 
 public class Excercise
 {
-    //3つ以上の数でもできるように
-
-    public static void main( String[] args )throws IOException{
-
-       
+    public static void main( String[] args )throws IOException{      
         
-        Scanner scan = new Scanner(System.in);
+        //Scanner scan = new Scanner(System.in);
 
-        System.out.print("数字を入力してください＞");
-        int suji = scan.nextInt();
-        System.out.print("文字を入力してください＞");
-        String moji = scan.next();
+        //System.out.print("何の段？＞");
+        //int dan = scan.nextInt();
+        //System.out.print("文字を入力してください＞");
+        //String moji = scan.next();
 
- //       int z =Integer.parseInt(br.readLine());
-//     triangle(10,"P");
-  //      triangle(25,"★");
-        triangle(suji,moji);
+        //int z =Integer.parseInt(br.readLine());
+        //triangle(10,"P");
+        //triangle(25,"★");
 
 
-    }
-   
-    static void triangle(int n,String s){
-        for(int t=0;t<=n;t++){
-            for(int y=0;y<t;y++){
-                System.out.print(s);
-                
-            }
-            System.out.println("");   
-            
+        for(int dan=1;dan<10;dan++){
+            kuku(dan);
         }
+    }
+static void kuku(int kari){
+    for(int kake=1;kake<10;kake++){
+        System.out.printf("%2d",kari*kake);
+        System.out.print(","); 
+    }
+System.out.println("");
+            
     }
 }
