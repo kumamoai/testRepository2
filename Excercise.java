bbe545a720a339a3db297bee1526fa6746fc2fd6
kumamoai <kumamoai0820@gmail.com>
2021-08-31 21:09:35 +0900

    クラス１

--- Excercise.java
+++ Excercise.java
@@ -1,32 +1,27 @@
 import java.io.*;
 import java.text.Normalizer.Form;
 
-public class Excercise{
-    public static void main( String[] args ){
+class Dog{
+    private String Name; //インスタンスフィールド
 
-        int from = 10;
-        int to = 20;
-        for(int a = (from+1) ; a<= (to+1) ; a++){
-            fibnum(a);   
-        }
+    public void SetName(String nm){  //メソッド
+        Name = nm;
+    } 
 
+    public void ShowProfile(){
+        System.out.println("名前は"+Name+"です");
     }
 
-    public static void fibnum (int num){ //フィボナッチ数列のnum項目を返すメソッド
 
-        int one = 0;
-        int next = 1;
+}
 
-        for(int i=0;i<100;i++){
 
-            if(i+1==num){
-                System.out.println(one);
-            }
-        
-            int nextnext = one + next;      
-            one=next;
-            next = nextnext;
-            
-        }
+public class Excercise{
+    public static void main( String[] args ){
+        Dog dog1 = new Dog(); //インスタンス作成
+
+        dog1.SetName("tubasa");
+
+        dog1.ShowProfile();
     }
 }
