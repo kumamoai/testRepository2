80b692f6db1127ae864fbe92f24b3a62c54fb228
kumamoai <kumamoai0820@gmail.com>
2021-09-01 22:47:01 +0900

    年齢の情報を追加

--- Excercise.java
+++ Excercise.java
@@ -3,6 +3,13 @@ import java.text.Normalizer.Form;
 
 class Dog{
     private String Name; //インスタンスフィールド
+    private int Age;
+
+
+    public void SetAge(int ag){
+        Age = ag;
+    }
+
 
     public void SetName(String nm){  //メソッド
         Name = nm;
@@ -10,6 +17,7 @@ class Dog{
 
     public void ShowProfile(){
         System.out.println("名前は"+Name+"です");
+        System.out.println("年齢は"+Age+"です");
     }
 
 
@@ -21,6 +29,7 @@ public class Excercise{
         Dog dog1 = new Dog(); //インスタンス作成
 
         dog1.SetName("tubasa");
+        dog1.SetAge(30);
 
         dog1.ShowProfile();
     }
