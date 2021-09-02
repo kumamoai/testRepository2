e59d1087bd06a6fa587f83d2898c36fccafdb36b
kumamoai <kumamoai0820@gmail.com>
2021-09-02 23:06:57 +0900

    インスタンス追加

--- Excercise.java
+++ Excercise.java
@@ -5,11 +5,9 @@ class Dog{
     private String Name; //インスタンスフィールド
     private int Age;
 
-
     public void SetAge(int ag){
         Age = ag;
     }
-
 
     public void SetName(String nm){  //メソッド
         Name = nm;
@@ -19,18 +17,19 @@ class Dog{
         System.out.println("名前は"+Name+"です");
         System.out.println("年齢は"+Age+"です");
     }
-
-
 }
-
 
 public class Excercise{
     public static void main( String[] args ){
         Dog dog1 = new Dog(); //インスタンス作成
+        Dog dog2 = new Dog();
 
-        dog1.SetName("tubasa");
-        dog1.SetAge(30);
-
+        dog1.SetName("Mario");
+        dog1.SetAge(26);
         dog1.ShowProfile();
+
+        dog2.SetName("Luigi");
+        dog2.SetAge(24);
+        dog2.ShowProfile();
     }
 }
